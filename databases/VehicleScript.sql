d68435b131e4bc3aee6ef5851927a011b88cbf0a
j1mmo <36548626+j1mmo@users.noreply.github.com>
2019-10-04 13:47:01 +0100

    Create VehicleScript.sql

---
+++ databases/VehicleScript.sql
@@ -0,0 +1,12 @@
+Drop table Vehicle;
+create table Vehicle
+    (VehicleType char(10),
+    currentvalue number(8,2));
+insert into vehicle values ('Bus', 12500.00);
+insert into vehicle values ('Car', 9987);
+/*
+    comment type 1
+*/
+select * from vehicle;
+
+--comment type 2
