822234b9ea128a3e7256c8b197bd708b6d4a96b9
j1mmo <36548626+j1mmo@users.noreply.github.com>
2019-10-07 11:47:36 +0100

    Delete isSorted.java

--- Data Structures & algorithms/sorting/isSorted.java
+++
@@ -1,8 +0,0 @@
-public static boolean isArraySorted(int a[])
-    // check if an array is sorted
-    {
-        for(int i = 1;i < a.length; i++){
-            if(a[i-1] > a[i]) return false;
-        }
-        return true;
-    }
