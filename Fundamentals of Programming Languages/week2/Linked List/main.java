7e1d7a9bec21fb6cbe506bbf669f337a2dec8ec2
j1mmo <36548626+j1mmo@users.noreply.github.com>
2019-10-10 10:56:36 +0100

    Create main.java

---
+++ Fundamentals of Programming Languages/week2/Linked List/main.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.Iterator;
+
+public class week2 {
+    public static void main(String[] args) throws IOException {
+    ListNode n = new ListNode(243);
+    ListNode x = new ListNode(564);
+    }
+}
