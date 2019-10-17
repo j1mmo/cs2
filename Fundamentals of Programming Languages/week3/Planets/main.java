e4c4041db888ea4018501be2e73cc42d2a5568df
j1mmo <36548626+j1mmo@users.noreply.github.com>
2019-10-17 16:16:12 +0100

    Update main.java

--- Fundamentals of Programming Languages/week3/Planets/main.java
+++ Fundamentals of Programming Languages/week3/Planets/main.java
@@ -1,27 +1,26 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args){
-        Planet Mercury = new Planet( 4879.4, 57.91, "Mercury");
-        Planet Earth = new Planet(12742,149.6, "Earth");
-        Planet Mars = new Planet(6779, 227.9, "Mars");
-        Planet Uranus = new Planet(50724, 2871, "Uranus");
-        Planet Saturn = new Planet(116460, 1434, "Saturn");
-        Planet Neptune = new Planet(49244, 4495, "Naptune");
-        Planet Jupiter = new Planet(139820, 778.5, "Jupiter");
-        Planet Venus = new Planet(12104, 108.2, "Venus");
-        ArrayList<Planet> solarSystem = new ArrayList<Planet>();
-        solarSystem.add(Mercury);
-        solarSystem.add(Earth);
-        solarSystem.add(Mars);
-        solarSystem.add(Uranus);
-        solarSystem.add(Saturn);
-        solarSystem.add(Neptune);
-        solarSystem.add(Jupiter);
-        solarSystem.add(Venus);
-        for (Planet p : solarSystem) {
-            p.getStats();
+    public static void main(String[] args) {
+        ArrayList<Planet> solarSystem = new ArrayList<>();
+        String[] names = {"Mercury",
+                "Venus",
+                "Earth",
+                "Mars",
+                "Jupiter",
+                "Saturn",
+                "Uranus",
+                "Neptune"
+        };
+        double[] radius = {2439.7, 6051.8, 6371, 3389.7, 69911, 58232, 25362, 24622};
+        double[] au = {0.39, 0.723, 1, 1.524, 5.203, 9.539, 19.18, 30.06};
+        double[] mass = {0.330, 4.87, 5.97, 0.642, 1898, 568, 86.8, 102};
+        for(int i = 0 ; i < 7; i++){
+            Planet p = new Planet(names[i], au[i], mass[i], radius[i]);
+            solarSystem.add(p);
+        }
+        for(Planet p : solarSystem){
+            p.printData();
         }
     }
-
 }
