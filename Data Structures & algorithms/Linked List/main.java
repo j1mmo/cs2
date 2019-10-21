2da92bccfb4806e4134aa5f4b731a34a0d8a2c31
j1mmo <36548626+j1mmo@users.noreply.github.com>
2019-10-21 11:54:25 +0100

    Create main.java

---
+++ Data Structures & algorithms/Linked List/main.java
@@ -0,0 +1,13 @@
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        LList test = new LList();
+        test.AddItem("test");
+        test.AddItem("test2");
+        test.InsertItem(0,"bob");
+        test.ListAllIItem();
+    }
+
+}
